0f219b1349eeab055f008f640dd859160cd19738
tissera.pablo <tissera.pablo@3601870e-5f09-8026-f928-dc6d88e15e94>
2012-10-30 03:39:38 +0000

    Informe De Tareas Desviadas Por Facturacion - parte 2 sin datos para probar aun

--- Proyectos/CellProjectManager/grails-app/controllers/business/informe/InformeDeTareasDesviadasPorFacturacionController.groovy
+++ Proyectos/CellProjectManager/grails-app/controllers/business/informe/InformeDeTareasDesviadasPorFacturacionController.groovy
@@ -23,8 +23,8 @@ class InformeDeTareasDesviadasPorFacturacionController {
             render(view: "step1")
         }else{
              
-            def desde = params.desde ? new Date().parse("dd/MM/yyyy", params.desde):null
-            def hasta = params.hasta ? new Date().parse("dd/MM/yyyy", params.hasta): null
+            def desde = params.desde ? new Date().parse("dd/MM/yyyy", params.desde_value) : null
+            def hasta = params.hasta ? new Date().parse("dd/MM/yyyy", params.hasta_value) : null
             def monto = params.monto
             
             
@@ -43,15 +43,62 @@ class InformeDeTareasDesviadasPorFacturacionController {
                                
                                 def diasDesvio = (tar.fechaInicioReal - tar.fechaInicio) + (tar.fechaFinReal?tar.fechaFinReal - tar.fechaFin:0)
                                 def porcentajeDesvio = (tar.fechaFin - tar.fechaInicio) * diasDesvio / 100
-                                datos << [proyecto: solicitud.proyecto,
+                                solicitudes << [proyecto: solicitud.proyecto,
                                     cliente: solicitud.proyecto.cliente, 
+                                    solicitud: solicitud,
                                     montoSolicitud: proyecto.totalPOs(),
-                                    porcentajeDesvio: porcentajeDesvio,
-                                    cuadrilla: solicitud.cuadrilla]
+                                    porcentajeDesvio: porcentajeDesvio]
                             }
                         }
                     }
                 }                
+            }
+            def auxSolicitudesResultMap = [:]
+                       
+            solicitudes.each{ solic->
+                def key = "${solic.solicitud.id}"
+                if(auxSolicitudesResultMap.containsKey(key)){
+                    auxSolicitudesResultMap[key].sumDesvio += solic.porcentajeDesvio
+                    auxSolicitudesResultMap[key].totalTareas++
+                    auxSolicitudesResultMap[key].porcentajeDesvio = auxSolicitudesResultMap[key].sumDesvio / auxSolicitudesResultMap[key].totalTareas
+                }else{
+                    auxSolicitudesResultMap[key] = [proyecto: solic.proyecto,
+                        cliente: solic.cliente, 
+                        solicitud: solic.solicitud,
+                        montoSolicitud: solic.montoSolicitud,
+                        porcentajeDesvio: solic.porcentajeDesvio,
+                        sumDesvio: solic.porcentajeDesvio,
+                        totalTareas: 1]
+                }                
+            }
+            def auxProyectosResultMap = [:]
+            
+            auxSolicitudesResultMap.each{ solicGroup->
+                
+                def item = solicGroup.value
+                def key = "${item.proyecto.id}"
+                if(auxProyectosResultMap.containsKey(key)){
+                    auxProyectosResultMap[key].sumDesvio += item.porcentajeDesvio
+                    auxProyectosResultMap[key].totalTareas+=item.totalTareas
+                    auxProyectosResultMap[key].totalMontoProyecto+=item.montoSolicitud
+                    auxProyectosResultMap[key].totalSolicitudes++
+                    auxProyectosResultMap[key].porcentajeDesvio = auxSolicitudesResultMap[key].sumDesvio / auxSolicitudesResultMap[key].totalSolicitudes
+                }else{
+                    auxProyectosResultMap[key] = [proyecto: item.proyecto,
+                        cliente: item.cliente, 
+                        porcentajeDesvio: item.porcentajeDesvio,         
+                        totalTareas: item.totalTareas,
+                        sumDesvio: item.porcentajeDesvio,
+                        totalMontoProyecto: item.montoSolicitud,
+                        totalSolicitudes: 1]
+                }  
+                
+            }
+            
+            auxProyectosResultMap.each{
+                def item = it.value
+                System.println(item)
+                datos << item
             }
             
             session.resultReport = datos
--- Proyectos/CellProjectManager/grails-app/views/acontecimiento/create.gsp
+++ Proyectos/CellProjectManager/grails-app/views/acontecimiento/create.gsp
@@ -9,8 +9,7 @@
 	<body>
 		<a href="#create-acontecimiento" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
 		<div class="nav" role="navigation">
-			<ul>
-				<li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
+			<ul>				
 				<li><g:link class="list" action="list"><g:message code="default.list.label" args="[entityName]" /></g:link></li>
 			</ul>
 		</div>
